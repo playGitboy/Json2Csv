6d46c126e92ed83dd3fd0cd06d1a69bb98076812
playGitboy <72111157+playGitboy@users.noreply.github.com>
2023-06-30 22:31:41 +0800

    修复BUG；支持桌面端双击运行

--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"runtime"
+	"strconv"
 	"strings"
-    "strconv"
 
 	"github.com/ecoshub/jin"
 	"github.com/gogf/gf/container/gmap"
@@ -44,9 +46,7 @@ func getMaxNode(listMap *gmap.ListMap) string {
 // 兼容非数组对象数据提取{"items":{"a1":{"title":"one","name":"test"},"b2":{"title":"two","name":"test2"}}}
 func writeObjToCSVFile(obj []string, csvHeader []string, outputFileName string) error {
 	file, err := os.Create(outputFileName)
-	if err != nil {
-		return err
-	}
+	checkErr(err)
 	defer file.Close()
 	file.WriteString("\xEF\xBB\xBF")
 
@@ -92,9 +92,7 @@ func writeArrayToCSVFile(listMap *gmap.ListMap, node string, csvHeader []string,
 		return fmt.Errorf(" > 数据区域不是一个数组！")
 	}
 	file, err := os.Create(outputFileName)
-	if err != nil {
-		return err
-	}
+	checkErr(err)
 	defer file.Close()
 	file.WriteString("\xEF\xBB\xBF")
 
@@ -155,7 +153,6 @@ func splitString(s string, myStrings []rune) []string {
 	return strings.FieldsFunc(s, Split)
 }
 
-
 func process(jPath string) {
 	if isFileExist(jPath) == false {
 		fmt.Printf(" > 指定文件不存在：%s\n", jPath)
@@ -163,10 +160,7 @@ func process(jPath string) {
 	}
 	csvFilePath := strings.Split(jPath, filepath.Ext(jPath))[0] + ".csv"
 	bJsonFile, err := ioutil.ReadFile(jPath)
-	if err != nil {
-		fmt.Printf(" > %s 读取错误： %v\n", jPath, err)
-		return
-	}
+	checkErr(err)
 
 	// 如果设置-k参数，则按szkey指定路径提取json
 	if len(szData) > 0 {
@@ -191,42 +185,37 @@ func process(jPath string) {
 	}
 
 	listMap, err := readJSONFile(bJsonFile)
-	if err != nil {
-		fmt.Printf(" > %s 读取错误： %v\n", jPath, err)
-		return
-	}
+	checkErr(err)
 
 	var csvHeader, objValues []string
 	maxNode := getMaxNode(listMap)
 	// csv表头仅首行写入一次；用于后续保留key顺序
-  if szKeys == ""{
-   	if maxNode == "" {
-  		// 兼容处理非数组对象数据提取{"items":{"a1":{"title":"one","name":"test"},"b2":{"title":"two","name":"test2"}}}
-  		objValues, err = jin.GetValues(bJsonFile)
-  		if err != nil {
-  			fmt.Println(err)
-  			return
-  		}
-  		csvHeader, err = jin.GetKeys([]byte(objValues[iIndex-1]))
-  	} else {
-  		fmt.Printf(" > 数据节点： %s\n", maxNode)
-  		// 因为map和json都无序，想保留json键顺序采用第三方库jin
-  		csvHeader, err = jin.GetKeys(bJsonFile, maxNode, strconv.Itoa(iIndex-1))
-  	}
-  	if err != nil {
-  		fmt.Printf(" > %s 读取错误： %v\n", jPath, err)
-  		flag.Usage()
-  		return
-  	}
-  }else{
-      csvHeader = splitString(szKeys,[]rune{'/',','})
-      if len(csvHeader)==0{
-        fmt.Printf(" > %s 读取错误，-k指定字段名不存在！\n", jPath)
-    		flag.Usage()
-    		return
-      }
-  }
-  fmt.Printf(" > %s 字段列表： %v\n", jPath, csvHeader)
+
+	if szKeys != "" {
+		csvHeader = splitString(szKeys, []rune{'/', ','})
+		if len(csvHeader) == 0 {
+			fmt.Printf(" > %s 读取错误，-k指定字段名不存在！\n", jPath)
+			flag.Usage()
+			return
+		}
+	}
+
+	if maxNode == "" {
+		// 兼容处理非数组对象数据提取{"items":{"a1":{"title":"one","name":"test"},"b2":{"title":"two","name":"test2"}}}
+		objValues, err = jin.GetValues(bJsonFile)
+		checkErr(err)
+		if len(csvHeader) == 0 {
+			csvHeader, err = jin.GetKeys([]byte(objValues[iIndex-1]))
+		}
+	} else {
+		fmt.Printf(" > 数据节点： %s\n", maxNode)
+		if len(csvHeader) == 0 {
+			// 因为map和json都无序，想保留json键顺序采用第三方库jin
+			csvHeader, err = jin.GetKeys(bJsonFile, maxNode, strconv.Itoa(iIndex-1))
+		}
+	}
+	checkErr(err)
+	fmt.Printf(" > %s 字段列表： %v\n", jPath, csvHeader)
 
 	if maxNode == "" {
 		err = writeObjToCSVFile(objValues, csvHeader, csvFilePath)
@@ -234,10 +223,75 @@ func process(jPath string) {
 		err = writeArrayToCSVFile(listMap, maxNode, csvHeader, csvFilePath)
 	}
 	if err != nil {
-		fmt.Printf(" > CSV文件写入错误： %v\n\n", err)
+		fmt.Printf(" > CSV文件写入错误： %v\n", err)
 	} else {
-		fmt.Printf(" > CSV文件成功写入： %s\n\n", csvFilePath)
+		fmt.Printf(" > CSV文件成功写入： %s\n", csvFilePath)
 	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Printf(" > 读取错误： %v\n", err)
+		flag.Usage()
+		return
+	}
+}
+
+func isRunFromCommandLine() (bool, error) {
+	parentProcessID := os.Getppid()
+	parentProcess, err := os.FindProcess(parentProcessID)
+	if err != nil {
+		return false, err
+	}
+
+	parentProcessName, err := getProcessName(parentProcess.Pid)
+	if err != nil {
+		return false, err
+	}
+
+	if isShell(parentProcessName) {
+		return true, nil
+	}
+	return false, nil
+}
+
+func getProcessName(pid int) (string, error) {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("wmic", "process", "where", "processid="+strconv.Itoa(pid), "get", "name")
+	case "darwin", "linux":
+		cmd = exec.Command("ps", "-p", strconv.Itoa(pid), "-o", "comm=")
+	default:
+		return "", fmt.Errorf("unsupported platform: %s", runtime.GOOS)
+	}
+
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	processName := strings.TrimSpace(string(output))
+	return processName, nil
+}
+
+func isShell(processName string) bool {
+	shells := []string{
+		"bash",
+		"sh",
+		"zsh",
+		"powershell",
+		"cmd",
+	}
+
+	for _, shell := range shells {
+		if strings.Contains(strings.ToLower(processName), shell) {
+			return true
+		}
+	}
+
+	return false
 }
 
 var (
@@ -245,7 +299,7 @@ var (
 	bVersion bool
 	szData   string
 	iIndex   int
-  szKeys   string
+	szKeys   string
 )
 
 func init() {
@@ -253,17 +307,20 @@ func init() {
 	flag.BoolVar(&bVersion, "v", false, "显示版本信息")
 	flag.StringVar(&szData, "d", "", "设置Json中数据区域所处路径，如'-d root.topics.data'")
 	flag.IntVar(&iIndex, "i", 1, "指定从第N个对象中提取字段名")
-  flag.StringVar(&szKeys, "k", "", "设置Json数据字段名称(分隔符'/'或','，优先级高于-i参数)，如'-k title/url/type'")
+	flag.StringVar(&szKeys, "k", "", "设置Json数据字段名称(分隔符'/'或','，优先级高于-i参数)，如'-k title/url/type'")
 }
 
 func main() {
+	isCmdLine, err := isRunFromCommandLine()
+	checkErr(err)
+
 	flag.Parse()
 	if bhelp {
 		flag.Usage()
 		return
 	}
 	if bVersion {
-		fmt.Println(" > 版本：v0.3\n > 主页：https://github.com/playGitboy/Json2Csv")
+		fmt.Println(" > 版本：v0.4\n > 主页：https://github.com/playGitboy/Json2Csv")
 		return
 	}
 
@@ -271,12 +328,15 @@ func main() {
 		for _, jsonFilePath := range flag.Args() {
 			process(jsonFilePath)
 		}
-	} else {
-		fmt.Println(" > Json2Csv：请指定JSON格式文件路径（支持批量）...")
-		fmt.Println(" > Json2Csv [-d data.items] data.json data2.txt ...")
- 		fmt.Println(" > Json2Csv [-d data.items] [-k title/url] data.json ...")
-    fmt.Println(" > Json2Csv [-d data.items] [-i 3] data.json ...")
-		flag.Usage()
+		return
 	}
-	//fmt.Scanln()
+	fmt.Println(" > Json2Csv：请指定JSON格式文件路径（支持批量）...")
+	fmt.Println(" > Json2Csv [-d data.items] data.json data2.txt ...")
+	fmt.Println(" > Json2Csv [-d data.items] [-k title/url] data.json ...")
+	fmt.Println(" > Json2Csv [-d data.items] [-i 3] data.json ...")
+	flag.Usage()
+	if !isCmdLine {
+		fmt.Println("\n > 请在终端/命令行使用本程序！")
+		fmt.Scanln()
+	}
 }
